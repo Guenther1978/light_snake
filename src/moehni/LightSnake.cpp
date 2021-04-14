502f3ab73cd55d9aeb4b8b3adb10ab70e6d927f6
Guenther Beulen <guenther.beulen@web.de>
2021-04-14 16:47:32 +0200

    Add Serial.print-function for debugging.

--- src/moehni/LightSnake.cpp
+++ src/moehni/LightSnake.cpp
@@ -4,7 +4,7 @@ void LightSnake::setup()
 {
   pwm.begin();
   pwm.setPWMFreq(1600);
-  
+
   for (uint8_t i = 0; i < NUMBER_OF_LEDS; i ++)
   {
     led[i].setNumber(i);
@@ -24,11 +24,22 @@ void LightSnake::loop()
     {
       led[i].changePointer();
       led[i].pointer2int();
+      Serial.println("New Intensity:");
+      Serial.print(led[i].getNumber());
+      Serial.print("\t");
+      Serial.print(led[i].getIntensity());
+      Serial.println();
       if (led[i].getDarkerHasChanged())
       {
         led[i].setSpeedControlDuration(random(1, DURATION_MAX));
+        Serial.print("\t");
+        Serial.println("New Duration:");
+        Serial.print(led[i].getNumber());
+        Serial.print("\t");
+        Serial.print(led[i].getSpeedControlDuration());
+        Serial.println();
       }
-      pwm.setPWM(led[i].getNumber(), 0, led[i].getIntensity());  
+      pwm.setPWM(led[i].getNumber(), 0, led[i].getIntensity());
     }
   }
   while (millis() < (old_millis + DELAY_TIME));
