5d48feeb386fd6765ba31b578b4501aca6a16cd9
Guenther Beulen <guenther.beulen@web.de>
2021-04-14 20:45:24 +0200

    Output only on demand.

--- src/moehni/LightSnake.cpp
+++ src/moehni/LightSnake.cpp
@@ -24,24 +24,34 @@ void LightSnake::loop()
     {
       led[i].changePointer();
       led[i].pointer2int();
-      Serial.println("New Intensity:");
-      Serial.print(led[i].getNumber());
-      Serial.print("\t");
-      Serial.print(led[i].getIntensity());
-      Serial.println();
       if (led[i].getDarkerHasChanged())
       {
-        led[i].setSpeedControlDuration(random(1, DURATION_MAX));
-        Serial.print("\t");
-        Serial.println("New Duration:");
-        Serial.print(led[i].getNumber());
-        Serial.print("\t");
-        Serial.print(led[i].getSpeedControlDuration());
-        Serial.println();
+        led[i].setSpeedControlDuration(random(DURATION_MAX) + 1);
       }
       pwm.setPWM(led[i].getNumber(), 0, led[i].getIntensity());
     }
   }
   while (millis() < (old_millis + DELAY_TIME));
   old_millis = millis();
+  if (Serial.available())
+    {
+      if (Serial.read() == 'd')
+        {
+          Serial.println();
+          Serial.println("number\tintensity\tdarker\tduration\tcounter");
+          for(int i = 0; i < NUMBER_OF_LEDS; i++)
+            {
+              Serial.print(led[i].getNumber());
+              Serial.print("\t");
+              Serial.print(led[i].getIntensity());
+              Serial.print("\t\t");
+              Serial.print(led[i].getDarker());
+              Serial.print("\t");
+              Serial.print(led[i].getSpeedControlDuration());
+              Serial.print("\t\t");
+              Serial.print(led[i].getSpeedControlCounter());
+              Serial.println();
+            }
+        }
+    }
 }
